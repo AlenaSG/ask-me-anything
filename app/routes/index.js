c56095c8c539397cb0902982855cafd46ccbd761
Alena Golovina <alenasgolovina@gmail.com>
2017-08-11 16:50:34 -0700

    add routes to the application

---
+++ app/routes/index.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+var rentals = [{
+  id: 1,
+  owner: "Veruca Salt",
+  city: "San Francisco",
+  type: "Estate",
+  bedrooms: 15,
+  image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+}, {
+  id: 2,
+  owner: "Mike TV",
+  city: "Seattle",
+  type: "Condo",
+  bedrooms: 1,
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
+}];
+
+export default Ember.Route.extend({
+  model() {
+    return rentals;
+  },
+});
